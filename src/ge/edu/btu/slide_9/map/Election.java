6d4ed185b711449b9e748572e7dcacc55bba7407
Soso Gobronidze <gobronidze.ioseb@gmail.com>
2020-10-29 21:00:37 +0400

    changes in slide 9

--- src/ge/edu/btu/slide_9/map/Election.java
+++ src/ge/edu/btu/slide_9/map/Election.java
@@ -1,17 +1,22 @@
 package ge.edu.btu.slide_9.map;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Election {
 
     private Map<String, Long> candidatesMap = new HashMap<>();
 
-    public void vote(String candidate) {
-        if (!candidatesMap.containsKey(candidate)) {
+    public void initCandidates(List<String> candidates) {
+        for (String candidate : candidates) {
             candidatesMap.put(candidate, 0L);
         }
-        candidatesMap.put(candidate, candidatesMap.get(candidate) + 1);
+    }
+
+    public void vote(String candidate) {
+        long votes = candidatesMap.get(candidate);
+        candidatesMap.put(candidate, votes + 1);
     }
 
     public void printResult() {
--- src/ge/edu/btu/slide_9/map/Runner.java
+++ src/ge/edu/btu/slide_9/map/Runner.java
@@ -1,9 +1,20 @@
 package ge.edu.btu.slide_9.map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
+        List<String> candidates = new ArrayList<>();
+        candidates.add("Test 1001");
+        candidates.add("Test 1002");
+        candidates.add("Test 1003");
+        candidates.add("Test 1004");
+
         Election election = new Election();
+        election.initCandidates(candidates);
+
         election.vote("Test 1001");
         election.vote("Test 1003");
         election.vote("Test 1002");
@@ -13,6 +24,7 @@ public class Runner {
         election.vote("Test 1003");
         election.vote("Test 1001");
         election.vote("Test 1002");
+
         election.printResult();
     }
 }
